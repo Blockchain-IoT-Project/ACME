be6ef9ccef81ba0108f39441b006defb0357aa34
github <github@lupusalix.de>
2020-01-30 13:33:54 -0500

    ADDED: Addchannel script

---
+++ network/scripts/createChannel.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# import utils
+. scripts/utils.sh
+
+createChannel() {
+	CHANNEL_NAME=$1
+	setGlobals 0 1
+
+	if [ -z "$CORE_PEER_TLS_ENABLED" -o "$CORE_PEER_TLS_ENABLED" = "false" ]; then
+                set -x
+		peer channel create -o orderer.acme.org:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel_$(echo $CHANNEL_NAME).tx >&log.txt
+		res=$?
+                set +x
+	else
+				set -x
+		peer channel create -o orderer.acme.org:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel_$(echo $CHANNEL_NAME).tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
+		res=$?
+				set +x
+	fi
+	cat log.txt
+	verifyResult $res "Channel creation failed"
+	echo "===================== Channel '$CHANNEL_NAME' created ===================== "
+	echo
+}
+
+echo $1
+CHANNEL_NAME=$1
+echo "Creating channel $CHANNEL_NAME..."
+createChannel $CHANNEL_NAME
+
