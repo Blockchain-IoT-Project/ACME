196bd65b6cb410c59cc23761b9e4a3457288634a
qwertxzy <leo@qwertxzy.me>
2020-01-30 21:30:50 +0100

    Fixing ccp-generate.sh paths

--- network/ccp-generate.sh
+++ network/ccp-generate.sh
@@ -32,8 +32,8 @@ ORG=1
 P0PORT=7051
 P1PORT=8051
 CAPORT=7054
-PEERPEM=crypto-config/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
-CAPEM=crypto-config/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
+PEERPEM=crypto-config/peerOrganizations/org1.acme.org/tlsca/tlsca.org1.acme.org-cert.pem
+CAPEM=crypto-config/peerOrganizations/org1.acme.org/ca/ca.org1.acme.org-cert.pem
 
 echo "$(json_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org1.json
 echo "$(yaml_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org1.yaml
@@ -42,8 +42,8 @@ ORG=2
 P0PORT=9051
 P1PORT=10051
 CAPORT=8054
-PEERPEM=crypto-config/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
-CAPEM=crypto-config/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
+PEERPEM=crypto-config/peerOrganizations/org2.acme.org/tlsca/tlsca.org2.acme.org-cert.pem
+CAPEM=crypto-config/peerOrganizations/org2.acme.org/ca/ca.org2.acme.org-cert.pem
 
 echo "$(json_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org2.json
 echo "$(yaml_ccp $ORG $P0PORT $P1PORT $CAPORT $PEERPEM $CAPEM)" > connection-org2.yaml
