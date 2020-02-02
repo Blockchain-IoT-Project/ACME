aa059038f310eb3361f09f58b95b60456e3770c4
Saibot101 <46537353+Saibot101@users.noreply.github.com>
2020-02-02 05:55:13 -0800

    Add files via upload

--- QueryChaincode/src/Config/Config.java
+++ QueryChaincode/src/Config/Config.java
@@ -16,18 +16,18 @@ public class Config {
 
 	public static final String ADMIN_PASSWORD = "adminpw";
 	
-	public static final String CHANNEL_CONFIG_PATH = "config/channel.tx";
+	public static final String CHANNEL_CONFIG_PATH = "config/channel_s1.tx";
 	
-	public static final String ORG1_USR_BASE_PATH = "crypto-config" + File.separator + "peerOrganizations" + File.separator
-			+ "org1.example.com" + File.separator + "users" + File.separator + "Admin@org1.example.com"
+	public static final String ORG1_USR_BASE_PATH = ".." + File.separator + "network" + File.separator + "crypto-config" +  File.separator
+			+ "org1.acme.org" + File.separator + "users" + File.separator + "Admin@org1.acme.org"
 			+ File.separator + "msp";
 	
-	public static final String ORG2_USR_BASE_PATH = "crypto-config" + File.separator + "peerOrganizations" + File.separator
-			+ "org2.example.com" + File.separator + "users" + File.separator + "Admin@org2.example.com"
+	public static final String ORG2_USR_BASE_PATH = ".." + File.separator + "network" + File.separator + "crypto-config" +  File.separator
+			+ "org2.acme.org" + File.separator + "users" + File.separator + "Admin@org2.example.org"
 			+ File.separator + "msp";
 	
 	public static final String ORG1_USR_ADMIN_PK = ORG1_USR_BASE_PATH + File.separator + "keystore";
-	public static final String ORG1_USR_ADMIN_CERT = ORG1_USR_BASE_PATH + File.separator + "admincerts";
+	public static final String ORG1_USR_ADMIN_CERT = ORG1_USR_BASE_PATH + File.separator + "signcerts";
 
 	public static final String ORG2_USR_ADMIN_PK = ORG2_USR_BASE_PATH + File.separator + "keystore";
 	public static final String ORG2_USR_ADMIN_CERT = ORG2_USR_BASE_PATH + File.separator + "admincerts";
@@ -38,31 +38,31 @@ public class Config {
 	
 	public static final String ORDERER_URL = "grpc://localhost:7050";
 	
-	public static final String ORDERER_NAME = "orderer.example.com";
+	public static final String ORDERER_NAME = "orderer.acme.org";
 	
-	public static final String CHANNEL_NAME = "mychannel";
+	public static final String CHANNEL_NAME = "s1";
 	
-	public static final String ORG1_PEER_0 = "peer0.org1.example.com";
+	public static final String ORG1_PEER_0 = "peer0.org1.acme.org";
 	
 	public static final String ORG1_PEER_0_URL = "grpc://localhost:7051";
 	
-	public static final String ORG1_PEER_1 = "peer1.org1.example.com";
+	public static final String ORG1_PEER_1 = "peer1.org1.acme.org";
 	
 	public static final String ORG1_PEER_1_URL = "grpc://localhost:7056";
 	
-    public static final String ORG2_PEER_0 = "peer0.org2.example.com";
+    public static final String ORG2_PEER_0 = "peer0.org2.acme.org";
 	
-	public static final String ORG2_PEER_0_URL = "grpc://localhost:8051";
+	public static final String ORG2_PEER_0_URL = "grpc://localhost:9051";
 	
-	public static final String ORG2_PEER_1 = "peer1.org2.example.com";
+	public static final String ORG2_PEER_1 = "peer1.org2.acme.org";
 	
 	public static final String ORG2_PEER_1_URL = "grpc://localhost:8056";
 	
 	public static final String CHAINCODE_ROOT_DIR = "chaincode";
 	
-	public static final String CHAINCODE_1_NAME = "fabcar";
+	public static final String CHAINCODE_1_NAME = "acme_cc_s1";
 	
-	public static final String CHAINCODE_1_PATH = "github.com/fabcar";
+	public static final String CHAINCODE_1_PATH = "github.com/chaincode";
 	
 	public static final String CHAINCODE_1_VERSION = "1";
 
